c51151151c0c11be2204528acfd18a36d6c0c39d
Rens Jaspers <rens.jaspers@gmail.com>
2020-08-13 14:55:00 +0200

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import { Component } from "@angular/core";
+import * as faker from "faker";
+import { DataService } from "../services/data.service";
+faker.seed(1);
 
+interface Person {
+  name: string;
+  birthDate: Date;
+  motto: string;
+  email: string;
+  phone: string;
+}
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
+  private persons: Person[] = Array.from(new Array(150)).map(() => ({
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    birthDate: faker.date.between("1910", "1999"),
+    motto: faker.hacker.phrase(),
+    email: faker.internet.email(),
+    phone: faker.phone.phoneNumber(),
+  }));
+  displayedPersons = this.persons.slice();
+  constructor() {}
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  search() {
+    this.displayedPersons = this.persons.filter((x) => x);
   }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
-
 }
